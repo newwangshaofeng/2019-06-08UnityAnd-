b504091e06a7e863e198b4a7fe184689b98ae334
王绍峰 <1693476459@qq.com>
2019-06-08 16:39:16 +0800

    ParallelLinq的用法,以及StopWatch计算运行时间

--- 05.Demo(TPL)/Program.cs
+++ 05.Demo(TPL)/Program.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _05.Demo_TPL_
 {
+    /// <summary>
+    /// 编写异步方法
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-
+            //调用异步测试方法
+            Test1();
 
             Console.ReadKey();
+        }
+
+        static async void Test1()
+        {
+            string str = await F1Async();
+            Console.WriteLine(str);
+        }
+
+
+        static Task<string> F1Async()
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                return "F2";
+            });
         }
     }
 }
---
+++ 07.LinqDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> dates=new List<int>();
+
+             Random rd=new Random();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                dates.Add(rd.Next());
+            }
+
+            //计时器
+            Stopwatch watch=new Stopwatch();
+            Stopwatch watch2=new Stopwatch();
+            watch.Start();
+
+            var c = dates.Where(d => d % 3 == 0);
+            Console.WriteLine(c.Count());
+
+
+            watch.Stop();
+
+            //PLinq
+            watch2.Start();
+            var c2 = dates.AsParallel().Where(d => d % 3 == 0);
+            Console.WriteLine(c2.Count());
+
+
+            watch2.Stop();
+           
+
+            //打印出运行花了多长时间(ms)
+            Console.WriteLine("Linq"+watch.ElapsedMilliseconds+"ms");
+            Console.WriteLine("PLinq"+watch2.ElapsedMilliseconds+"ms");
+
+            Console.ReadKey();
+        }
+    }
+}
